72d1dc32fd40b499376f16fdc1b18709b8df8334
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-08-02 09:16:36 -0300

    Feat: the fonts was imported

--- src/components/title.styled.ts
+++ src/components/title.styled.ts
@@ -3,6 +3,6 @@ import styled from "styled-components/native";
 
 export const Title = styled(Text)`
   font-size: 24px;
-  font-family: 'Poppins-Black', sans-serif;
+  font-family: 'Poppins_700Bold';
   /* font-family: "Victor Mono", monospace; */
 `;
